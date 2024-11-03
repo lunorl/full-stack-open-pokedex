3f55731a79f2eb586f2c80e92241140ea080d219
lunorl <nicolas.huot@videotron.ca>
2024-11-03 18:44:20 -0500

    a

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
+app.get("version", (req, res) => {
+  res.send("1");
+});
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
