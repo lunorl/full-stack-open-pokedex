14027b22259e6071fb68dc2ba1d9e40c8370ef4f
lunorl <nicolas.huot@videotron.ca>
2024-11-03 19:05:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("version", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
   if (true) throw "error...  ";
   res.send("1");
 });
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`);
 });
